90f3dba4b7291dacbc4c14d5a68239726090be55
Bohdan Zakusylo <bohdan.zakusylo@student.nhlstenden.com>
2026-02-25 23:49:50 +0100

    wow deployment works

--- Routers/HttpFuncs/RequestImageAZFunction.cs
+++ Routers/HttpFuncs/RequestImageAZFunction.cs
@@ -38,7 +38,7 @@ public sealed class RequestImageAZFunction
                 correlationId, parentJobId);
 
             var response = req.CreateResponse(HttpStatusCode.Accepted);
-            await response.WriteAsJsonAsync(new { jobId = parentJobId, status = "I GUES WORKS" }, ct);
+            await response.WriteAsJsonAsync(new { jobId = parentJobId, status = "STARTED" }, ct);
 
             _logger.LogInformation(
                 "RequestImageGeneration accepted. CorrelationId={CorrelationId}, JobId={JobId}",
